ce789be84f8b9178f9ebdc2259c2ee255ce447c2
Nikhil Chaudhari <10090206+voldemorte@users.noreply.github.com>
2020-04-11 10:54:25 +0530

    Bugfix/kuberntes provider (#6)
    * EKS cluster fails to apply aws-auth config map
    
    * EKS provider version

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -45,6 +45,21 @@ resource "aws_iam_policy" "eks_worker_assume_role" {
 
 ###############################
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
 module "eks" {
   source                               = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v11.0.0"
   cluster_name                         = var.cluster_name
